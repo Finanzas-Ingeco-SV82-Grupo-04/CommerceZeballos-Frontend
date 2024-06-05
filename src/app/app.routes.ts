41c539b38264e59c4c632d8ad5abeafa66cab824
Anthony Piero Avalos Santos <104176063+Anthony2000AA@users.noreply.github.com>
2024-06-04 22:10:04 -0500

    fix: clean project html and add environment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,23 @@
 import { Routes } from '@angular/router';
+import { AdminSigninComponent } from './commerce/security/components/admin-signin/admin-signin.component';
+import { ClientSigninComponent } from './commerce/security/components/client-signin/client-signin.component';
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
+
+    {path: '', redirectTo: '/login', pathMatch: 'full'},//default route
+
+    {path: 'login', component: ClientSigninComponent},
+
+
+    {path: 'dashboard-admin', 
+        children: [
+            {path: 'login', component: AdminSigninComponent},
+            //{path: 'users', component: UsersComponent},
+            //{path: 'roles', component: RolesComponent},
+           // {path: 'permissions', component: PermissionsComponent},
+            //{path: 'settings', component: SettingsComponent},
+        ]
+    },
+
+   
 ];
