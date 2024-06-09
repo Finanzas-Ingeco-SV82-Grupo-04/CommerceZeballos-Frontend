de4b72cd9479a80eb99b5d96c0c156e22c781b71
Claudio Moreno Rosales <u20191e800@upc.edu.pe>
2024-06-08 23:46:07 -0500

    feat: showing clients list

--- src/app/commerce/list-clients/components/list-clients/list-clients.component.ts
+++ src/app/commerce/list-clients/components/list-clients/list-clients.component.ts
@@ -1,12 +1,74 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {Client} from '../../model/list-clients.model';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatFormFieldModule, MatLabel } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatSortModule } from "@angular/material/sort";
+import { FormsModule } from "@angular/forms";
+import {ListClientsService} from "../../services/list-clients.service";
+import {BrowserModule} from "@angular/platform-browser";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-list-clients',
   standalone: true,
-  imports: [],
-  templateUrl: './list-clients.component.html',
-  styleUrl: './list-clients.component.css'
-})
-export class ListClientsComponent {
+  imports: [
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatSortModule,
+    FormsModule,
+    MatLabel,
+    //BrowserModule,
+    HttpClientModule,
 
+  ],
+  templateUrl: './list-clients.component.html',
+  styleUrls: ['./list-clients.component.css']
+})
+
+export class ListClientsComponent implements OnInit {
+  searchTerm = '';
+  clients: Client[] = [];
+  displayedColumns: string[] = ['name', 'dni', 'actions'];
+  dataSource = new MatTableDataSource<Client>(this.clients);
+
+  constructor(private clientService: ListClientsService) {}
+
+  ngOnInit(): void {
+    this.getAllClients();
+  }
+
+  getAllClients(): void {
+    this.clientService.getAllClients().subscribe((clients: Client[]) => {
+      this.clients = clients;
+      this.dataSource.data = clients;
+    });
+  }
+
+  deleteUser(client: Client): void {
+    if (confirm(`Are you sure you want to delete ${client.firstname} ${client.lastname}?`)) {
+      this.clientService.deleteClient(client.dni).subscribe(() => {
+        this.clients = this.clients.filter(c => c.dni !== client.dni);
+        this.dataSource.data = this.clients;
+        alert('Client deleted successfully');
+      }, error => {
+        alert('Failed to delete client');
+      });
+    }
+  }
+
+  viewDetails(client: Client): void {
+    console.log('Ver detalles:', client);
+  }
+
+  search(): void {
+    console.log('Buscar:', this.searchTerm);
+  }
 }
--- src/app/commerce/list-clients/services/list-clients.service.ts
+++ src/app/commerce/list-clients/services/list-clients.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../../../environments/environment.development";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Client} from "../model/list-clients.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListClientsService {
 
-  constructor() { }
+  private BASE_URL = environment.baseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getAllClients(): Observable<Client[]> {
+    return this.http.get<Client[]>(`${this.BASE_URL}clients/findAll`);
+  }
+
+  deleteClient(dni: string): Observable<void> {
+    return this.http.delete<void>(`${this.BASE_URL}clients/delete/${dni}`);
+  }
 }
