a4edcc6dd325ddb87f76fc6be7f5ed17e4c967a0
MiguelC <sackito992@gmail.com>
2024-06-24 17:21:13 -0500

    feat: add purchase report table as a balance report

--- src/app/commerce/purchase-report/components/purchase-report-table/purchase-report-table.component.ts
+++ src/app/commerce/purchase-report/components/purchase-report-table/purchase-report-table.component.ts
@@ -1,19 +1,57 @@
-import { Component } from '@angular/core';
-import {MatColumnDef, MatHeaderCell, MatHeaderRow, MatHeaderRowDef, MatTable} from "@angular/material/table";
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../service/data.service';
+import { MatTableModule} from "@angular/material/table";
+import { RouterOutlet, RouterLink,  RouterLinkActive} from '@angular/router';
+import { MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'app-purchase-report-table',
   standalone: true,
-    imports: [
-        MatColumnDef,
-        MatHeaderCell,
-        MatHeaderRow,
-        MatHeaderRowDef,
-        MatTable
-    ],
+  imports: [MatTableModule, RouterLink, RouterLinkActive, MatButtonModule],
   templateUrl: './purchase-report-table.component.html',
-  styleUrl: './purchase-report-table.component.css'
+  styleUrls: ['./purchase-report-table.component.css']
 })
-export class PurchaseReportTableComponent {
+export class PurchaseReportTableComponent implements OnInit {
   displayedColumns: string[] = ['fecha', 'compras', 'pagos', 'saldo'];
+  dataSource: any;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getData().subscribe(data => {
+      this.dataSource = data;
+    });
+  }
+
+  sortAscending = true;
+  sortPurchasesAscending = true;
+
+  sortTableByDate() {
+    const sortedDataSource = [...this.dataSource].sort((a, b) => {
+      const dateA = new Date(a.fecha);
+      const dateB = new Date(b.fecha);
+
+      if (this.sortAscending) {
+        return dateA.getTime() - dateB.getTime();
+      } else {
+        return dateB.getTime() - dateA.getTime();
+      }
+    });
+
+    this.dataSource = sortedDataSource;
+    this.sortAscending = !this.sortAscending;
+  }
+
+  sortTableByPurchases() {
+    const sortedDataSource = [...this.dataSource].sort((a, b) => {
+      if (this.sortPurchasesAscending) {
+        return a.compras - b.compras;
+      } else {
+        return b.compras - a.compras;
+      }
+    });
+
+    this.dataSource = sortedDataSource;
+    this.sortPurchasesAscending = !this.sortPurchasesAscending;
+  }
 }
